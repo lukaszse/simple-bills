167504abdd00ba79d14e9b5a3fd51e8bb7b9a5f7
lseremak <llseremak@gmail.com>
2022-12-11 15:20:03 +0100

    add unit test to libs

--- simple-bills-commons/lib/src/test/groovy/pl/com/seremak/simplebills/commons/utils/TransactionBalanceUtilsSpec.groovy
+++ simple-bills-commons/lib/src/test/groovy/pl/com/seremak/simplebills/commons/utils/TransactionBalanceUtilsSpec.groovy
@@ -28,4 +28,27 @@ class TransactionBalanceUtilsSpec extends Specification {
         -344.03           | ActionType.DELETION | 1000.33             || 1344.36
         150               | ActionType.DELETION | 200                 || 50
     }
+
+    def "should uprate category usage correctly"() {
+
+        given: "prepare TransactionEventDto"
+        def transactionEventDto =
+                TransactionBalanceSpecUtils.prepareTransactionEventDto(transactionAmount as BigDecimal, actionType)
+
+        when: "invoke updateCategoryUsage method"
+        def balanceAfterUpdate =
+                TransactionBalanceUtils.updateCategoryUsage(balanceBeforeUpdate as BigDecimal, transactionEventDto)
+
+        then: "should calculate category usage correctly"
+        balanceAfterUpdate == expectedBalanceAfterUpdate
+
+        where:
+        transactionAmount | actionType          | balanceBeforeUpdate || expectedBalanceAfterUpdate
+        -100              | ActionType.CREATION | 5000                || 5100
+        100.22            | ActionType.CREATION | 232.22              || 132
+        -99.99            | ActionType.UPDATE   | 5000                || 5099.99
+        99.99             | ActionType.UPDATE   | 5000                || 4900.01
+        -344.03           | ActionType.DELETION | 1000.33             || 656.30
+        150               | ActionType.DELETION | 200                 || 350
+    }
 }
