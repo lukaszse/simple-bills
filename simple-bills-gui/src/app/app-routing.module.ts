9ce47696909231cd4b8fc0ea6a1808797d74df0c
lseremak <llseremak@gmail.com>
2022-12-25 19:55:59 +0100

    change css to scss

--- simple-bills-gui/src/app/app-routing.module.ts
+++ simple-bills-gui/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { AssetsComponent } from "./content/assets/assets.component";
 import { LoginComponent } from "./content/login/login.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
   {path: 'transactions', component: TransactionsComponent},
